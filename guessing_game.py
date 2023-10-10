95765b54c86075d21e82b099c3a21fd75a15c5dd
Derek Marion <derek.j.marion@gmail.com>
2023-10-09 17:29:52 -0700

    Create the GuessingGame class, instantiate it, and make a few calls to demonstrate functionality

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,30 @@
-class GuessingGame():
+class GuessingGame:
+    def __init__(self, number) -> None:
+        self.number = number
+        self.issolved = False
+
+    def guess(self, guess):
+        if guess < self.number:
+            print("low")
+            self.issolved = False
+        elif guess > self.number:
+            print("high")
+            self.issolved = False
+        else:
+            print("correct")
+            self.issolved = True
+
+    def solved(self):
+        print(self.issolved)
+        return self.issolved
+
+game = GuessingGame(10)
+
+game.solved() 
+
+game.guess(5)  
+game.guess(20)
+game.solved()  
+
+game.guess(10) 
+game.solved()
